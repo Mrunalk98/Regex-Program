2aed2e8b7c385dc4f65948bb16c55466778db0ac
Kmrunal20 <mrunal.kurhade@digiplusit.com>
2020-10-05 09:44:46 +0530

    [mrunal] Add. Added feature to create regex pattern to validate pin code

--- RegexProgram/Program.cs
+++ RegexProgram/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Text.RegularExpressions;
 
 namespace RegexProgram
 {
@@ -7,6 +8,21 @@ namespace RegexProgram
         static void Main(string[] args)
         {
             Console.WriteLine("Welcome to Regex Program!");
+            string pinCodePattern = "^[1-9]{1}[0-9]{2}\\s{0,1}[0-9]{3}$";
+            Regex regex = new Regex(pinCodePattern);
+
+            Console.Write("Enter a pin code : ");
+            var pinCode = Console.ReadLine();
+
+            if (regex.IsMatch(pinCode))
+            {
+                Console.WriteLine("Valid Pin Code");
+            }
+            else
+            {
+                Console.WriteLine("Invalid Pin Code");
+
+            }
         }
     }
 }
